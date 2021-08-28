55eccae6046ee4b0dc05e4d045878d45ddf11fdb
Alex <lisatthu35@gmail.com>
2021-08-29 01:19:22 +0700

    Update server.js

--- express/server.js
+++ express/server.js
@@ -11,6 +11,24 @@ router.get('/', (req, res) => {
   res.write('<h1>Hello from Express.js!</h1>');
   res.end();
 });
+
+router.get('/countdown', function(req, res) {
+  res.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive'
+  })
+  countdown(res, 10)
+})
+
+function countdown(res, count) {
+  res.write("data: " + count + "\n\n")
+  if (count)
+    setTimeout(() => countdown(res, count-1), 1000)
+  else
+    res.end()
+}
+
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
