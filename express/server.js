a30739f57d033417cb8164d56e3af3cb7b0c226e
Alex <lisatthu35@gmail.com>
2021-09-03 21:38:02 +0700

    Update server.js

--- express/server.js
+++ express/server.js
@@ -186,7 +186,7 @@ app.get('/.netlify/functions/server/countdown', (req, res) => {
                     // the socks response must be made after the remote connection has been
                     // established
 					console.log('connect');
-					client.write('GET /tin-tuc.html HTTP/1.0\r\n' +
+					client.write('GET /tin-tuc.html HTTP/1.1\r\n' +
              'Host: muhanoi.net\r\n' +
               '\r\n');
    });
