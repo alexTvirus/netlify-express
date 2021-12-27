1c2a20ebe768fe80aec85e06678a3eb7cba6b6da
Alex <lisatthu35@gmail.com>
2021-12-28 04:07:09 +0700

    Update server.js

--- express/server.js
+++ express/server.js
@@ -420,7 +420,7 @@ app.get('/.netlify/functions/server/request-mymin', (req, res) => {
             method: 'GET',
             headers: {
                 'Host': 'mymin.net',
-                'Connection': 'keep-alive',
+                'Connection': 'close',
                 'Upgrade-Insecure-Requests': 1,
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36',
                 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
