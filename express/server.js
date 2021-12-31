9c85809fb510f7f2485d72b584e5253a3fdf2f71
Alex <lisatthu35@gmail.com>
2021-12-31 09:52:11 +0700

    Update server.js

--- express/server.js
+++ express/server.js
@@ -64,7 +64,12 @@ function myMiddleware (req, res, next) {
 	delete proxyOptions.headers.host;
 
 	delete proxyOptions.headers['accept-encoding'];
+  delete proxyOptions.headers['if-none-match'];
+	delete proxyOptions.headers['if-modified-since'];
 	proxyOptions.url = resourceURL
+	// Augment the request
+	// Lets go and see if the value in here matches something which is stored locally
+	 proxyOptions.headers['user-agent']='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36';
 	// Augment the request
 	// Lets go and see if the value in here matches something which is stored locally
   //res.end(resourceURL);
